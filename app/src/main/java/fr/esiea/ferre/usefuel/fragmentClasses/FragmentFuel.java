94a24559bdefca62a0387e45cbeb9fe886c0840e
Mogulu <robin.ferre@gmail.com>
2017-07-06 13:04:26 +0700

    Demo book + map
    simple link entre orderfuel et mapactivities pour une demo

--- app/src/main/java/fr/esiea/ferre/usefuel/fragmentClasses/FragmentFuel.java
+++ app/src/main/java/fr/esiea/ferre/usefuel/fragmentClasses/FragmentFuel.java
@@ -1,14 +1,23 @@
 package fr.esiea.ferre.usefuel.fragmentClasses;
 
+import android.content.Intent;
 import android.os.Bundle;
 import android.app.Fragment;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.Button;
 
+import java.util.Map;
+
+import fr.esiea.ferre.usefuel.LoginActivity;
+import fr.esiea.ferre.usefuel.MapActivity;
 import fr.esiea.ferre.usefuel.R;
 
-public class FragmentFuel extends Fragment {
+public class FragmentFuel extends Fragment implements View.OnClickListener {
+
+    private Button buttonBook;
+
     @Override
     public View onCreateView(LayoutInflater inflater,
                              ViewGroup container,
@@ -16,6 +25,29 @@ public class FragmentFuel extends Fragment {
 
         // Inflate the layout for this fragment
         View view =  inflater.inflate(R.layout.fragment_fuel, container, false);
+
+
+        buttonBook =(Button) view.findViewById(R.id.button_book);
+        buttonBook.setOnClickListener(this);
+
+
         return view;
+    }
+
+    public void onClick(View v) {
+        // TODO Auto-generated method stub
+        switch(v.getId()){
+
+            case R.id.button_book :
+                getActivity().finish();
+                Intent intent = new Intent(getActivity(), MapActivity.class);
+                startActivity(intent);
+
+                break;
+
+            default:
+                break;
+
+        }
     }
 }
